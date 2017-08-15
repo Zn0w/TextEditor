3e131bd0b5530f8f168bd30b85344cec616e1c48
Daniil Blagiy <blagiy.daniil@gmail.com>
2017-08-15 15:16:21 +0300

    added basic window rendering and structure

---
+++ src/com/znow/aquatexteditor/Main.java
@@ -0,0 +1,11 @@
+package com.znow.aquatexteditor;
+
+import com.znow.aquatexteditor.gui.MainWindow;
+
+public class Main {
+
+	public static void main(String[] args) {
+		MainWindow window = new MainWindow();
+	}
+
+}
